4c74d9075338abd12f738eeb5de0807d32c4fb94
caside <ambientstalkir@gmail.com>
2021-07-09 15:43:36 +0700

    fix

--- dbt_sugar/core/connectors/clickhouse_connector.py
+++ dbt_sugar/core/connectors/clickhouse_connector.py
@@ -8,6 +8,7 @@ from typing import Dict
 import sqlalchemy
 
 from dbt_sugar.core.connectors.base import BaseConnector
+from dbt_sugar.core.logger import GLOBAL_LOGGER as logger
 
 
 class ClickhouseConnector(BaseConnector):
@@ -28,6 +29,7 @@ class ClickhouseConnector(BaseConnector):
             connection_params (Dict[str, str]): Dict containing database connection
                 parameters and credentials.
         """
+        logger.info(f"connectionparams={connection_params}")
         self.connection_url = sqlalchemy.engine.url.URL(
             drivername="clickhouse",
             host=connection_params.get("host", str()),
@@ -36,4 +38,11 @@ class ClickhouseConnector(BaseConnector):
             database=connection_params.get("database", str()),
             port=connection_params.get("port", str()),
         )
+        logger.info(f"connectionurl={self.connection_url}")
+        conurl=f"clickhouse://{connection_params.get('user', str())}:{connection_params.get('password', str())}@{connection_params.get('host', str())}:{connection_params.get('port', str())}/{connection_params.get('database', str())}"
+        self.connection_url=conurl
+        logger.info(f"connection_url={conurl}")
         self.engine = sqlalchemy.create_engine(self.connection_url)
+
+
+
